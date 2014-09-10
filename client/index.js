aa81c4388ae8ae062acf3140a5f3613a11d41909
rtmalone <rtylermalone@gmail.com>
2014-09-09 23:00:10 -0500

    Zombie pets/angular excercise

--- client/index.js
+++ client/index.js
@@ -1,17 +1,50 @@
 (function(){
   'use strict';
 
-  angular.module('mean-template', [])
-  .controller('MainController', ['$scope', '$interval', function($scope, $interval){
-    var occupations = ['Superheroes', 'Ninjas', 'Pirates', 'Vampires', 'Aliens', 'Dragons', 'Sharks with Lasers', 'Transformers', 'I am Groot'];
+  angular.module('zombie-pets', [])
+  .controller('MainController', ['$scope', function($scope){
 
-    $scope.title = 'Mean Template';
-    $scope.occupation = occupations[0];
+    $scope.title = 'Zombie Pets';
 
-    $interval(function(){
-      var rnd = Math.floor(Math.random() * occupations.length);
-      $scope.occupation = occupations[rnd];
-    }, 500);
+    $scope.weapon = {};
+    $scope.weapons = [];
+    $scope.pet = {health:100};
+    $scope.pets = [];
+
+    $scope.player1 = null;
+    $scope.player2 = null;
+
+    $scope.showCage = function(){
+      if ($scope.player1) {
+        return true;
+      }
+    };
+
+    $scope.setPlayer = function(num){
+      $scope['player' + num] = this.p; // << Pet which was just clicked on.
+    };
+
+    $scope.toggleWeapon = function(){
+      $scope.hideWeapon = !!!$scope.hideWeapon;
+    };
+
+    $scope.togglePet = function(){
+      $scope.hidePet = !!!$scope.hidePet;
+    };
+
+    $scope.addWeapon = function(){
+      //var w = $scope.weapon;
+      $scope.weapons.push($scope.weapon);
+      $scope.weapon = {};
+      $('#name').focus();
+    };
+
+    $scope.addPet = function(){
+      var index = $scope.pet.weapon * 1;
+      $scope.pet.weapon = $scope.weapons[index];
+      $scope.pets.push($scope.pet);
+      $scope.pet = {health:100};
+    };
+
   }]);
 })();
-
